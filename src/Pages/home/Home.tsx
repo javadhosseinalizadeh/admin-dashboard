188a52bb6b0c514d6c058551bfe5d87b7aa1392c
javad alizadeh <javadalizadeh1993@gmail.com>
2024-05-07 10:22:43 +0330

    pieChart

--- src/Pages/home/Home.tsx
+++ src/Pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import BarChartBox from '../../components/barChartBox/BarChartBox'
 import ChartBox from '../../components/chartBox/ChartBox'
+import PieChartBox from '../../components/pieChartBox/PieChartBox'
 import TopBox from '../../components/topBox/TopBox'
 import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from '../../data'
 import './home.scss'
@@ -10,7 +11,7 @@ const Home = () => {
       <div className="box box1"><TopBox /></div>
       <div className="box box2"><ChartBox {...chartBoxUser}/></div>
       <div className="box box3"><ChartBox {...chartBoxProduct}/></div>
-      <div className="box box4">B4</div>
+      <div className="box box4"><PieChartBox /></div>
       <div className="box box5"><ChartBox {...chartBoxConversion}/></div>
       <div className="box box6"><ChartBox {...chartBoxRevenue}/></div>
       <div className="box box7">B7</div>
