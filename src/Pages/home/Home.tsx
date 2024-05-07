f61ef1e9e1167a670a7c658ed7723548a61a9c1c
javad alizadeh <javadalizadeh1993@gmail.com>
2024-05-07 10:05:58 +0330

    Barchart component

--- src/Pages/home/Home.tsx
+++ src/Pages/home/Home.tsx
@@ -1,6 +1,7 @@
+import BarChartBox from '../../components/barChartBox/BarChartBox'
 import ChartBox from '../../components/chartBox/ChartBox'
 import TopBox from '../../components/topBox/TopBox'
-import { chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from '../../data'
+import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from '../../data'
 import './home.scss'
 const Home = () => {
   return (
@@ -13,8 +14,8 @@ const Home = () => {
       <div className="box box5"><ChartBox {...chartBoxConversion}/></div>
       <div className="box box6"><ChartBox {...chartBoxRevenue}/></div>
       <div className="box box7">B7</div>
-      <div className="box box8">B8</div>
-      <div className="box box9">B9</div>
+      <div className="box box8"><BarChartBox {...barChartBoxVisit}/></div>
+      <div className="box box9"><BarChartBox {...barChartBoxRevenue}/></div>
     </div>
   )
 }
